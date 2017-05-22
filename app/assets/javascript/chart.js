42611d1a19ef4a64cd840d872077c5d1988790d9
hirobs <hiro.bs@outlook.com>
2017-05-21 21:07:42 -0300

    Changed chart - finally working ( JANEIRO, FEVEREIRO, MARCO)

--- app/assets/javascript/chart.js
+++ app/assets/javascript/chart.js
@@ -1,276 +1,300 @@
-// Create the chart
+$(function () {
 
-Highcharts.chart('container', {
-  chart: {
-    type: 'column'
-  },
-  title: {
-    text: 'Relatório de Vendas'
-  },
-  subtitle: {
-    text: 'Clique na coluna para ver os dias'
-  },
-  xAxis: {
-    type: 'category'
-  },
-  yAxis: {
-    title: {
-      text: 'Total de Vendas'
-    }
+    // Create the chart
+    $('#grafico').highcharts({
+      chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Relatório de Vendas'
+        },
+        xAxis: {
+            type: 'category'
+        },
 
-  },
-  legend: {
-    enabled: false
-  },
-  plotOptions: {
-    series: {
-      borderWidth: 0,
-      dataLabels: {
-        enabled: true
-      }
-    }
-  },
+        legend: {
+            enabled: false
+        },
 
-  tooltip: {
-    headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-    pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
-  },
+        plotOptions: {
+            series: {
+                borderWidth: 0,
+                dataLabels: {
+                    enabled: true,
+                }
+            }
+        },
 
-  series: [{
-    name: 'Brands',
-    colorByPoint: true,
-    data: [{
-      name: 'Janeiro',
-      y: 56.33,
-      drilldown: 'Microsoft Internet Explorer'
-    }, {
-      name: 'Fevereiro',
-      y: 24.03,
-      drilldown: 'Chrome'
-    }, {
-      name: 'Março',
-      y: 10.38,
-      drilldown: 'Firefox'
-    }, {
-      name: 'Abril',
-      y: 4.77,
-      drilldown: 'Safari'
-    }, {
-      name: 'Maio',
-      y: 0.91,
-      drilldown: 'Opera'
-    }, {
-      name: 'Junho',
-      y: 0.2,
-      drilldown: null
-    }, {
-      name: 'Julho',
-      y: 10.38,
-      drilldown: null
-    }, {
-      name: 'Agosto',
-      y: 10.38,
-      drilldown: null
-    }, {
-      name: 'Setembro',
-      y: 10.38,
-      drilldown: null
-    }, {
-      name: 'Outubro',
-      y: 10.38,
-      drilldown: null
-    }, {
-      name: 'Novembro',
-      y: 10.38,
-      drilldown: null
-    }, {
-      name: 'Dezembro',
-      y: 10.38,
-      drilldown: null
-    }]
-  }],
-  drilldown: {
-    series: [{
-      name: 'Microsoft Internet Explorer',
-      id: 'Microsoft Internet Explorer',
-      data: [
-        [
-          'v11.0',
-          24.13
-        ],
-        [
-          'v8.0',
-          17.2
-        ],
-        [
-          'v9.0',
-          8.11
-        ],
-        [
-          'v10.0',
-          5.33
-        ],
-        [
-          'v6.0',
-          1.06
-        ],
-        [
-          'v7.0',
-          0.5
-        ]
-      ]
-    }, {
-      name: 'Chrome',
-      id: 'Chrome',
-      data: [
-        [
-          'v40.0',
-          5
-        ],
-        [
-          'v41.0',
-          4.32
-        ],
-        [
-          'v42.0',
-          3.68
-        ],
-        [
-          'v39.0',
-          2.96
-        ],
-        [
-          'v36.0',
-          2.53
-        ],
-        [
-          'v43.0',
-          1.45
-        ],
-        [
-          'v31.0',
-          1.24
-        ],
-        [
-          'v35.0',
-          0.85
-        ],
-        [
-          'v38.0',
-          0.6
-        ],
-        [
-          'v32.0',
-          0.55
-        ],
-        [
-          'v37.0',
-          0.38
-        ],
-        [
-          'v33.0',
-          0.19
-        ],
-        [
-          'v34.0',
-          0.14
-        ],
-        [
-          'v30.0',
-          0.14
-        ]
-      ]
-    }, {
-      name: 'Firefox',
-      id: 'Firefox',
-      data: [
-        [
-          'v35',
-          2.76
-        ],
-        [
-          'v36',
-          2.32
-        ],
-        [
-          'v37',
-          2.31
-        ],
-        [
-          'v34',
-          1.27
-        ],
-        [
-          'v38',
-          1.02
-        ],
-        [
-          'v31',
-          0.33
-        ],
-        [
-          'v33',
-          0.22
-        ],
-        [
-          'v32',
-          0.15
-        ]
-      ]
-    }, {
-      name: 'Safari',
-      id: 'Safari',
-      data: [
-        [
-          'v8.0',
-          2.56
-        ],
-        [
-          'v7.1',
-          0.77
-        ],
-        [
-          'v5.1',
-          0.42
-        ],
-        [
-          'v5.0',
-          0.3
-        ],
-        [
-          'v6.1',
-          0.29
-        ],
-        [
-          'v7.0',
-          0.26
-        ],
-        [
-          'v6.2',
-          0.17
-        ]
-      ]
-    }, {
-      name: 'Opera',
-      id: 'Opera',
-      data: [
-        [
-          'v12.x',
-          0.34
-        ],
-        [
-          'v28',
-          0.24
-        ],
-        [
-          'v27',
-          0.17
-        ],
-        [
-          'v29',
-          0.16
-        ]
-      ]
-    }]
-  }
+        series: [{
+            name: 'Ano',
+            colorByPoint: true,
+            data: [{
+                name: '2017',
+                y: 5,
+                drilldown: '2017'
+            }]
+        }],
+        drilldown: {
+            series: [{
+                id: '2017',
+                name: 'Mês',
+                data: [{
+                    name: 'Janeiro',
+                    y: 4,
+                    drilldown: 'janeiro'
+                },
+                {
+                    name:'Fevereiro',
+                    y:2,
+                    drilldown:'fevereiro'
+                },
+                {
+                    name:'Março',
+                    y:2,
+                    drilldown:'marco'
+                },
+                {
+                    name:'Abril',
+                    y:2,
+                    drilldown:'abril'
+                },
+                {
+                    name:'Maio',
+                    y:2,
+                    drilldown:'maio'
+                },
+                {
+                    name:'Junho',
+                    y:3,
+                    drilldown:'junho'
+                },
+                {
+                    name:'Julho',
+                    y:2,
+                    drilldown:'julho'
+                },
+                {
+                    name:'Agosto',
+                    y:7,
+                    drilldown:'agosto'
+                },
+                {
+                    name:'Setembro',
+                    y:2,
+                    drilldown:'setembro'
+                },
+                {
+                    name:'Outubro',
+                    y:2,
+                    drilldown:'outubro'
+                },
+                {
+                    name:'Novembro',
+                    y:5,
+                    drilldown:'novembro'
+                },
+                {
+                    name:'Dezembro',
+                    y:3,
+                    drilldown:'dezembro'
+                }
+                ]
+            }, {
+
+                id: 'janeiro',
+                data: [{name:"1",
+                        y:1},
+                       {name:"2",
+                       y:2},
+                       {name:"3",
+                        y:1},
+                        {name:"4",
+                        y:1},
+                       {name:"5",
+                       y:2},
+                       {name:"6",
+                        y:1},
+                        {name:"7",
+                        y:1},
+                       {name:"8",
+                       y:2},
+                       {name:"9",
+                        y:1},
+                        {name:"10",
+                        y:1},
+                       {name:"11",
+                       y:2},
+                       {name:"12",
+                        y:1},
+                        {name:"13",
+                        y:1},
+                       {name:"14",
+                       y:2},
+                       {name:"15",
+                        y:1},
+                        {name:"16",
+                        y:1},
+                       {name:"17",
+                       y:2},
+                       {name:"18",
+                        y:1},
+                        {name:"19",
+                        y:1},
+                       {name:"20",
+                       y:2},
+                       {name:"21",
+                        y:1},
+                        {name:"22",
+                        y:1},
+                       {name:"23",
+                       y:2},
+                       {name:"24",
+                        y:1},
+                        {name:"25",
+                        y:1},
+                       {name:"26",
+                       y:2},
+                       {name:"27",
+                        y:1},
+                        {name:"28",
+                        y:1},
+                       {name:"29",
+                       y:2},
+                       {name:"30",
+                        y:1},
+                        {name:"31",
+                        y:1}
+                       ]
+            },{
+
+                id: 'fevereiro',
+                data: [{name:"1",
+                        y:1},
+                       {name:"2",
+                       y:2},
+                       {name:"3",
+                        y:1},
+                        {name:"4",
+                        y:1},
+                       {name:"5",
+                       y:2},
+                       {name:"6",
+                        y:1},
+                        {name:"7",
+                        y:1},
+                       {name:"8",
+                       y:2},
+                       {name:"9",
+                        y:1},
+                        {name:"10",
+                        y:1},
+                       {name:"11",
+                       y:2},
+                       {name:"12",
+                        y:1},
+                        {name:"13",
+                        y:1},
+                       {name:"14",
+                       y:2},
+                       {name:"15",
+                        y:1},
+                        {name:"16",
+                        y:1},
+                       {name:"17",
+                       y:2},
+                       {name:"18",
+                        y:1},
+                        {name:"19",
+                        y:1},
+                       {name:"20",
+                       y:2},
+                       {name:"21",
+                        y:1},
+                        {name:"22",
+                        y:1},
+                       {name:"23",
+                       y:2},
+                       {name:"24",
+                        y:1},
+                        {name:"25",
+                        y:1},
+                       {name:"26",
+                       y:2},
+                       {name:"27",
+                        y:1},
+                        {name:"28",
+                        y:1},
+                       {name:"29",
+                       y:2}
+                    ]
+                },{
+
+                id: 'marco',
+                data: [{name:"1",
+                        y:1},
+                       {name:"2",
+                       y:2},
+                       {name:"3",
+                        y:1},
+                        {name:"4",
+                        y:1},
+                       {name:"5",
+                       y:2},
+                       {name:"6",
+                        y:1},
+                        {name:"7",
+                        y:1},
+                       {name:"8",
+                       y:2},
+                       {name:"9",
+                        y:1},
+                        {name:"10",
+                        y:1},
+                       {name:"11",
+                       y:2},
+                       {name:"12",
+                        y:1},
+                        {name:"13",
+                        y:1},
+                       {name:"14",
+                       y:2},
+                       {name:"15",
+                        y:1},
+                        {name:"16",
+                        y:1},
+                       {name:"17",
+                       y:2},
+                       {name:"18",
+                        y:1},
+                        {name:"19",
+                        y:1},
+                       {name:"20",
+                       y:2},
+                       {name:"21",
+                        y:1},
+                        {name:"22",
+                        y:1},
+                       {name:"23",
+                       y:2},
+                       {name:"24",
+                        y:1},
+                        {name:"25",
+                        y:1},
+                       {name:"26",
+                       y:2},
+                       {name:"27",
+                        y:1},
+                        {name:"28",
+                        y:1},
+                       {name:"29",
+                       y:2},
+                       {name:"30",
+                        y:1},
+                        {name:"31",
+                        y:1}
+                       ]
+            }
+            ]
+        }
+    })
 });
