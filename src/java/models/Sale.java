69c93bcc7c0d1d22ee9ad6d49972ba2a901ccfa7
hirobs <hiro.bs@outlook.com>
2017-07-04 19:55:19 -0300

    Add Saleitem and Sale

--- src/java/models/Sale.java
+++ src/java/models/Sale.java
@@ -1,6 +1,10 @@
 
 package models;
 
+import java.util.ArrayList;
+
 public class Sale {
+    private ArrayList<SaleItem> saleItem;
+    private double totalSale;
     
 }
---
+++ src/java/models/SaleItem.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author hirob
+ */
+public class SaleItem {
+    private int quantity;
+    private Product product;
+    private double total;
+    String url;
+}
