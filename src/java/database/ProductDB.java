8f532420eb7a041f7781e21805672abb7d134b68
amandarover <rover.amanda@gmail.com>
2017-07-05 23:01:47 -0300

    Create database connection with Products

--- src/java/database/ProductDB.java
+++ src/java/database/ProductDB.java
@@ -1,5 +1,135 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import models.Product;
+
 public class ProductDB {
+
+    public void insertProduct(Product product) throws SQLException {
+
+        String insertString =
+            "INSERT INTO schema_cat.PRODUCTS " +
+            "(imageUrl, name, description, unitOfMeasure, pricePerUnit, minimunStock, validateDate, stock)" +
+            "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+        try (PreparedStatement statement = DBServices.getConnection().prepareStatement(insertString)){
+            statement.setString(1, product.getImageUrl());
+            statement.setString(2, product.getName());
+            statement.setString(3, product.getDescription());
+            statement.setString(4, product.getUnitOfMeasure());
+            statement.setDouble(5, product.getPricePerUnit());
+            statement.setDouble(6, product.getMinimunStock());
+            statement.setDate(7, product.getValidateDate());
+            statement.setDouble(8, product.getStock());
+            
+            statement.execute(insertString);
+            
+            int rowsInserted = statement.executeUpdate();
+            if (rowsInserted > 0) {
+                System.out.println("A new user was inserted successfully!");
+            }
+            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro no insertProduct \n " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
     
+    public void updateProduct(Product product) throws SQLException {
+        String updateString =
+            "UPDATE schema_cat.PRODUCTS " +
+            "SET imageUrl = ? and " +
+            "SET name = ? and " +
+            "SET description = ? and " +
+            "SET unitOfMeasure = ? and " +
+            "SET pricePerUnit = ? and " +
+            "SET minimunStock = ? and " +
+            "SET validateDate = ? and " +
+            "SET stock = ? and " +
+            "WHERE idProducts = ?";
+
+        try (PreparedStatement statement = DBServices.getConnection().prepareStatement(updateString)){
+            statement.setString(1, product.getImageUrl());
+            statement.setString(2, product.getName());
+            statement.setString(3, product.getDescription());
+            statement.setString(4, product.getUnitOfMeasure());
+            statement.setDouble(5, product.getPricePerUnit());
+            statement.setDouble(6, product.getMinimunStock());
+            statement.setDate(7, product.getValidateDate());
+            statement.setDouble(8, product.getStock());
+            //preparestatement para chave primaria
+            int rowsUpdated = statement.executeUpdate(updateString);
+            //javautildate para javasqldate
+            
+            if (rowsUpdated > 0) {
+                System.out.println("A new user was updated successfully!");
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro no updateProduct \n " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    
+    public void deleteProduct(int idProduct) throws SQLException {
+        String deleteString = "DELETE FROM Pessoa WHERE idProducts = ?";
+
+        try (PreparedStatement statement = DBServices.getConnection().prepareStatement(deleteString)){
+            statement.setInt(1, idProduct);
+
+            int rowsDeleted = statement.executeUpdate(deleteString);
+
+            if (rowsDeleted > 0) {
+                System.out.println("A new user was deleted successfully!");
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro no deleteProduct \n " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    
+    public void selectAllProducts() throws SQLException {
+        String deleteString = "SELECT * FROM products";
+
+        try (PreparedStatement statement = DBServices.getConnection().prepareStatement(deleteString)){
+            ResultSet result = statement.executeQuery(deleteString);
+            //mostra por output os valores das tabelas para verificaçao se deu certo o/
+            int count = 0;
+
+            while (result.next()){
+                String name = result.getString("name");
+                double pricePerUnit = result.getDouble("pricePerUnit");
+                double validateDate = result.getDouble("validateDate");
+                String output = "Product #%d: %s - %d - %t";
+                System.out.println(String.format(output, ++count, name, pricePerUnit, validateDate));
+            }
+            //termina a amostragem
+            
+        } catch (Exception e ) {
+            JOptionPane.showMessageDialog(null, "Erro no selectAllProduct \n " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    
+    public void selectProduct(int idProduct) throws SQLException {
+        String selectString = "SELECT * FROM products WHERE idProducts = ? ";
+
+        try (PreparedStatement statement = DBServices.getConnection().prepareStatement(selectString)){
+            statement.setInt(1, idProduct);
+            ResultSet result = statement.executeQuery(selectString);
+            
+            String name = result.getString("name");
+            double pricePerUnit = result.getDouble("pricePerUnit");
+            double validateDate = result.getDouble("validateDate");
+            String output = "Product: %s - %d - %t";
+            System.out.println(String.format(output, name, pricePerUnit, validateDate));
+            
+        } catch (Exception e ) {
+            JOptionPane.showMessageDialog(null, "Erro no selectAllProduct \n " + e.getMessage());
+            e.printStackTrace();
+        }
+    }    
 }
