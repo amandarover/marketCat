66cb51594efd21f786c929192b4bb5fa5c99da8c
amandarover <rover.amanda@gmail.com>
2017-07-06 19:33:36 -0300

    Add idSale, dateSale and cpfCustomer to Sale model

--- src/java/models/Sale.java
+++ src/java/models/Sale.java
@@ -1,12 +1,23 @@
-
 package models;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Sale {
+    private long idSale;
     private ArrayList<SaleItem> saleItem;
     private double totalSale;
+    private Date saleDate;
+    private String cpfCustomer;
 
+    public long getIdSale() {
+        return idSale;
+    }
+
+    public void setIdSale(long idSale) {
+        this.idSale = idSale;
+    }
+    
     public ArrayList<SaleItem> getSaleItem() {
         return saleItem;
     }
@@ -22,7 +33,20 @@ public class Sale {
     public void setTotalSale(double totalSale) {
         this.totalSale = totalSale;
     }
-    
-    
-    
+
+    public Date getSaleDate() {
+        return saleDate;
+    }
+
+    public void setSaleDate(Date saleDate) {
+        this.saleDate = saleDate;
+    }
+
+    public String getCpfCustomer() {
+        return cpfCustomer;
+    }
+
+    public void setCpfCustomer(String cpfCustomer) {
+        this.cpfCustomer = cpfCustomer;
+    }
 }
--- src/java/models/SaleItem.java
+++ src/java/models/SaleItem.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package models;
 
-/**
- *
- * @author hirob
- */
 public class SaleItem {
     private int quantity;
     private Product product;
@@ -38,7 +29,4 @@ public class SaleItem {
     public void setTotal(double total) {
         this.total = total;
     }
-
-    
-    
 }
