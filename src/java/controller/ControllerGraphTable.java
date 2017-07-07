9cc95462f34a8ffd24b9e25798610fdd4c988556
hirobs <hiro.bs@outlook.com>
2017-07-07 15:39:59 -0300

    Starting Controller Graphic

--- src/java/controller/ControllerGraphTable.java
+++ src/java/controller/ControllerGraphTable.java
@@ -5,10 +5,19 @@
  */
 package controller;
 
-/**
- *
- * @author hiroshi.siqueira
- */
-public class ControllerGraphTable {
-    
+import database.ProductDB;
+import database.UserDB;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import models.User;
+
+@WebServlet("/ControllerGraphTable")
+public class ControllerGraphTable extends HttpServlet {
+
 }
